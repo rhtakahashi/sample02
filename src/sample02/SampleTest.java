9c4cfd8f1881a4bf3a5aafbe9ecbfb78a40681ee
Takahashi Ryouhei <rtakahashi@ap.equinix.com>
2018-04-16 21:48:13 +0900

    h1

---
+++ src/sample02/SampleTest.java
@@ -0,0 +1,11 @@
+package sample02;
+
+import static java.lang.System.out;
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		out.println("test");
+	}
+
+}
